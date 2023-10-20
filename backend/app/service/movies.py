cc3c497c89c4cff87731d2d4f2776aed509d3aec
Yunus Emre Terzi <86986919+yunusterzii@users.noreply.github.com>
2023-10-20 22:43:12 +0300

    Delete backend directory

--- backend/app/service/movies.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import random
-
-
-def getRandomMovies():
-    links = pd.read_csv("app\\data\\links.csv", index_col=False)
-    tmdbIDs = links["tmdbId"].to_list()
-    return random.sample(tmdbIDs, 10)
